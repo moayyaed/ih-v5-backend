d7e981b1a0ac45bfe5b47a507a0f019b61170004
nimagrolis <486016@gmail.com>
2020-01-27 15:35:23 +0300

    First commit

---
+++ app.js
@@ -0,0 +1,29 @@
+/**
+ * ih-backend-D
+ *
+ *  Main
+ */
+
+const util = require('util');
+
+require('./lib/init')
+  .startP(__dirname)
+  .then(() => {
+    require('./lib/webserver');
+
+    process.on('exit', () => {});
+
+    process.on('SIGINT', () => {
+      process.exit(0);
+    });
+
+    process.on('uncaughtException', err => {
+      console.log('ERR: uncaughtException ' + util.inspect(err));
+    });
+  })
+  .catch(e => {
+    console.log('ERR: FATAL ERROR. ' + util.inspect(e));
+    setTimeout(() => {
+      process.exit();
+    }, 500);
+  });
