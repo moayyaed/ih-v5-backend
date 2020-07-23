de49fd19bbe333bb1c5c14ea47ee81ec3f5be52e
nimagrolis <486016@gmail.com>
2020-07-23 13:20:20 +0300

    Add started messages

--- app.js
+++ app.js
@@ -16,54 +16,6 @@ const webserver = require('./lib/web/webserver');
 const resserver = require('./lib/resource/resourceserver');
 const EventEmitter = require('events');
 
-// const holder = new EventEmitter();
-/*
-init(__dirname)
-  .then(() => {
-    console.log('INFO: deviceserver start ' );
-    deviceserver(holder);
-    console.log('INFO: deviceserver started ' );
-  })
-  .then(() => {
-    console.log('INFO: resserver start ' );
-    resserver(holder);
-    console.log('INFO: resserver started ' );
-  })
-  .then(() => {
-    console.log('INFO: pluginserver start ' );
-    pluginserver(holder);
-    console.log('INFO: pluginserver started ' );
-  })
-  .then(() => {
-    console.log('INFO: sceneserver start ' );
-    sceneserver(holder);
-    console.log('INFO: sceneserver started ' );
-  })
-  .then(() => {
-    console.log('INFO: trendserver start ' );
-    trendserver(holder);
-    console.log('INFO: trendserver started ' );
-  })
-  .then(() => {
-    console.log('INFO: webserver start ' );
-    webserver(holder);
-    console.log('INFO: webserver started ' );
-  })
-  .catch(e => {
-    console.log('ERROR: FATAL ERROR. ' + util.inspect(e));
-    setTimeout(() => {
-      process.exit();
-    }, 500);
-  });
-   process.on('uncaughtException', err => {
-    console.log('ERROR: uncaughtException ' + util.inspect(err));
-  });
-
-  process.on('unhandledRejection', (reason, promise) => {
-    console.log('ERROR: Unhandled Rejection at:', promise, 'reason:', reason);
-  });
-
-*/
 (async () => {
   const holder = new EventEmitter();
 
@@ -78,31 +30,12 @@ init(__dirname)
  
   try {
     await init(__dirname);
-
-    console.log('INFO: deviceserver start ' );
     await deviceserver(holder);
-    console.log('INFO: deviceserver started ' );
-  
-  
-    console.log('INFO: resserver start ' );
     await resserver(holder);
-    console.log('INFO: resserver started ' );
-  
-    console.log('INFO: pluginserver start ' );
     await pluginserver(holder);
-    console.log('INFO: pluginserver started ' );
-  
-    console.log('INFO: sceneserver start ' );
     await sceneserver(holder);
-    console.log('INFO: sceneserver started ' );
-  
-    console.log('INFO: trendserver start ' );
     await trendserver(holder);
-    console.log('INFO: trendserver started ' );
- 
-    console.log('INFO: webserver start ' );
     await webserver(holder);
-    console.log('INFO: webserver started ' );
   } catch (err) {
     console.log('ERROR: Main App Exception ' + util.inspect(err));
     setTimeout(() => {
