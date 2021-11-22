277321454dfab466ffb06deda6c53af83a8e0afe
nimagrolis <486016@gmail.com>
2021-11-22 14:29:32 +0300

    v5.5.131-beta

---
+++ upgrade/up_5_6.js
@@ -0,0 +1,11 @@
+/**
+ * Project upgrade 5.5 => 5.6
+ *
+ * 1. Изменение наименования плагина интеграции applehomekit => applehome
+ *
+ *
+ */
+
+module.exports = async function(projectPath) {
+  //  Найти плагин
+}
